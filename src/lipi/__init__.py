d5d2df520b5cd480fef7e5ec456dc0bf3b3ea5e1
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-05 14:58:02 +0000

    Release v1.0.5

--- src/lipi/__init__.py
+++ src/lipi/__init__.py
@@ -9,10 +9,10 @@ from lipi.preprocessor import HindiPreprocessor
 from lipi.regression import run_regression_harness
 from lipi.text_pipeline import clean_extracted_text
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __all__ = [
-	"HindiPreprocessor",
-	"HindiLexiconCorrector",
-	"clean_extracted_text",
-	"run_regression_harness",
+    "HindiPreprocessor",
+    "HindiLexiconCorrector",
+    "clean_extracted_text",
+    "run_regression_harness",
 ]
