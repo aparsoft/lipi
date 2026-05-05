e2bdc72d62daeb64f124432f76ef6c6eee4975da
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-05 19:20:33 +0000

    Update: src/lipi/__init__.py
    ✨ Auto-committed on 2026-05-05 19:20:33

--- src/lipi/__init__.py
+++ src/lipi/__init__.py
@@ -9,7 +9,7 @@ from lipi.preprocessor import HindiPreprocessor
 from lipi.regression import run_regression_harness
 from lipi.text_pipeline import clean_extracted_text
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 __all__ = [
     "HindiPreprocessor",
     "HindiLexiconCorrector",
